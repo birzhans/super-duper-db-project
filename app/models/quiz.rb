cd89636cd9477785a63ccaaccc6f9dd380153dcc
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-03-11 04:00:53 +0600

    add validations

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,6 +3,10 @@ class Quiz < ApplicationRecord
   belongs_to :category
   has_many :questions, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, :category_id, presence: true
   validates :level, inclusion: 1..10
+
+  def ready?
+    self.questions.any? && self.questions.all? { |q| q.answers.any? }
+  end
 end
--- db/migrate/20210309132803_create_quizzes.rb
+++ db/migrate/20210309132803_create_quizzes.rb
@@ -1,12 +1,14 @@
 class CreateQuizzes < ActiveRecord::Migration[6.1]
   def change
     create_table :quizzes do |t|
-      t.string :title
+      t.string :title, null: false
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
       t.integer :level, null: false, default: 1
 
       t.timestamps
     end
+
+    add_index :quizzes, :title, unique: true
   end
 end
